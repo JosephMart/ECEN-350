a9b7938cb3c0198c3bcf4c588be29ee834f13a04
Joseph Martinsen <josephmart@lin12-424cvlb.ece.tamu.edu>
2017-10-30 16:02:10 -0500

    Data Memory Completed

--- Lab08/data_memory/DataMemory.v
+++ Lab08/data_memory/DataMemory.v
@@ -6,24 +6,20 @@ module DataMemory(ReadData, Address, WriteData,
                   MemoryRead, MemoryWrite, Clock);
 	input [63:0] WriteData, Address;
 	input MemoryRead, MemoryWrite, Clock;
-	output reg [63:0] ReadData;
+	output reg[63:0] ReadData;
 
-	reg [63:0] register [63:0];
+	reg [31:0] register [63:0];
 
-	//memory write only commits to the memory on the negative edge of a clock, where 
+	//memory write only commits to the memory on the negative edge of a clock 
 	always @(negedge Clock) begin
-		if (MemoryWrite == 1 && MemoryRead == 0)
+		if (MemoryWrite & ~MemoryRead) // Ensure no race conditions
 			register[Address] <= #20 WriteData;
-		else
-			ReadData <= #20 64'b0;
 	end
 
 	// read operations occur on the positive edge of the clock
 	always @(posedge Clock) begin
-		if (MemoryRead == 1 && MemoryWrite == 0)
-			ReadData <= #20 Address;
-		else
-			ReadData <= #20 64'b0;
+		if (MemoryRead & ~MemoryWrite) // Ensure no race conditions
+			ReadData <= #20 register[Address];
 	end
 	
 endmodule
