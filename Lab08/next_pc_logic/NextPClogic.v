d7182b3d5846871124a10c0d4cee7a7650deb432
Joseph Martinsen <josephmart@apollo3.ece.tamu.edu>
2017-11-05 18:50:53 -0600

    3/4 on nextpclogic baby

--- Lab08/next_pc_logic/NextPClogic.v
+++ Lab08/next_pc_logic/NextPClogic.v
@@ -10,10 +10,10 @@ module NextPClogic(NextPC, CurrentPC, SignExtImm64, Branch, ALUZero, Uncondbranc
 	always @(*) begin
 		PC_4 = CurrentPC + 64'b100;
 		if(Branch & ALUZero)
-          NextPC <= #2 (SignExtImm64 << 2) + PC_4;
-    else if (Uncondbranch)
-          NextPC <= #2 SignExtImm64 + CurrentPC;
-    else
-          NextPC <= #2 PC_4;
+			NextPC <= #2 (SignExtImm64 << 2) + CurrentPC ;
+		else if (Uncondbranch)
+			NextPC <= #2 SignExtImm64;
+		else
+			NextPC <= #2 PC_4;
 	end
 endmodule
